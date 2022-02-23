0d244f5d20bde08a52f9b2e69be35cee57b6903c
gitadmen <94184870+gitadmen@users.noreply.github.com>
2022-02-23 13:18:30 +0100

    asd

--- js/main.js
+++ js/main.js
@@ -11,19 +11,41 @@ $(document).ready(function () {
 
         if (animated) {
             let calc = window.innerWidth / 2 - 100;
-            $('#square').animate({ marginLeft: '0px' });
+            $('#square').animate(
+                {
+                    marginLeft: '0px',
+                    height: '200px',
+                    width: '200px',
+                    backgroundColor: "#aa0000",
+                },
+
+                {
+                    duration: 1000,
+                }
+
+            );
+
             animated = false;
 
         }
         else {
-            let calc = window.innerWidth / 2 - 100;
-            $('#square').animate({ marginLeft: calc + "px" });
+            let calc = window.innerWidth / 2 - 50;
+            $('#square').animate(
+
+                {
+                    marginLeft: calc + "px",
+                    height: '100px',
+                    width: '100px',
+                    backgroundColor: "#aa0000"
+                },
+
+                {
+                    duration: 1000,
+                }
+
+            );
             animated = true;
         }
 
-
-
     });
-
-
 })
